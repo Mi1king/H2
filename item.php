af1b9f2177a02cbcbe9802a52065d666267fa7c8
Mi1king <Razhel999@gmail.com>
2022-05-08 00:39:20 +0800

    require login when item detail page

--- item.php
+++ item.php
@@ -7,10 +7,14 @@ include 'nav.php';
 include 'includes/check-if-added.php';
 ?>
 
-<?php 
-$id=$_GET['id'];
-include("./template/_item.php");
+<?php
 
+if (!isset($_SESSION['email'])) {
+    header('location:login.php');
+} else {
+    $id = $_GET['id'];
+    include("./template/_item.php");
+}
 ?>
 
 <?php include("footer.php"); ?>

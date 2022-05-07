661a6bb32fd8449914d76501c47d13dc7c9d8b28
Mi1king <Razhel999@gmail.com>
2022-05-08 00:20:45 +0800

    update index page

--- index.php
+++ index.php
@@ -6,16 +6,6 @@ if (isset($_SESSION['email'])) {
     header('location:products.php');
 }
 ?>
-<!-- <html lang="en">
-  <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welcome | Health Hour</title>
-        <link href="CSS/bootstrap.css" rel="stylesheet">
-        <link href="CSS/style.css" rel="stylesheet">
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </head>
-<body style="padding-top:50px;"> -->
 <?php
 include 'nav.php';
 ?>
--- template/_index.php
+++ template/_index.php
@@ -3,53 +3,11 @@
         <div class="container">
             <center>
                 <div id="banner_content">
-                    <h1>We sell health.</h1>
-                    <p>Flat 40% OFF on premium brands </p>
+                    <h1>My the health be with you</h1>
                     <br />
                     <a href="products.php" class="btn btn-danger btn-lg active">Shop Now</a>
                 </div>
             </center>
-        </div>
-    </div>
-
-</div>
-
-<div class="container">
-    <div class="row text-center" id="item_list">
-        <div class="col-sm-4">
-            <a href="products.php#cameras">
-                <div class="thumbnail">
-                    <img src="img/1.jpg" alt="">
-                    <div class="caption">
-                        <h3>Cameras</h3>
-                        <p>Choose among the best available in the world.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col-sm-4">
-            <a href="products.php#watches">
-                <div class="thumbnail">
-                    <img src="img/10.jpg" alt="">
-                    <div class="caption">
-                        <h3>Watches</h3>
-                        <p>Original watches from the best brands.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col-sm-4">
-            <a href="products.php#shirts">
-                <div class="thumbnail">
-                    <img src="img/13.jpg" alt="">
-                    <div class="caption">
-                        <h3>Shirts</h3>
-                        <p>Our exquisite collection of shirts.</p>
-                    </div>
-                </div>
-            </a>
         </div>
     </div>
 </div>

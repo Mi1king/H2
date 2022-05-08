e4f9c67e74d7a2b9f45f0e004fb3b18daceaa1b2
yijun00 <82631780+yijun00@users.noreply.github.com>
2022-05-09 01:26:48 +0800

    Add files via upload

--- template/_account.php
+++ template/_account.php
@@ -40,8 +40,9 @@ WHERE
     $name = mysqli_fetch_array($result_user);
     echo '
     <div class="col-md-12 col-sm-6 home-feature">
+    <h4>'.$name["NAME"].'</h4>
       <img class="rounded-circle" style="width: 20rem;" src='.$name["PHOTO"].'>
-        <h4>'.$name["NAME"].'</h4>
+        
     </div>';
      
 echo "<div class='row'>

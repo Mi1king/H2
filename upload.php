b55fc2e7acd262838398640348bbdfbdf66664ba
yijun00 <82631780+yijun00@users.noreply.github.com>
2022-05-09 00:57:16 +0800

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,4 +1,9 @@
 <?php
+require_once("includes/common.php");
+if (!isset($_SESSION['email'])) {
+    header('location: index.php');
+}
+$user_id = $_SESSION['user_id'];
 $file = $_FILES['image'];
 $ext = strrchr($file['name'], '.');
 $newName = mt_rand(0000, 9999).time().$ext;
@@ -6,9 +11,9 @@ $path = 'img/'.$newName;
 $info = move_uploaded_file($file['tmp_name'], $path);
 
 require("includes/common.php");
-$query = "UPDATE  users SET photo = '$path' WHERE users.id = '4'";
+$query = "UPDATE  users SET photo = '$path' WHERE users.id = '$user_id'";
         mysqli_query($con, $query) or die($mysqli_error($con));
-$a = "SELECT users.photo AS PHOTO FROM users WHERE users.id = 4";
+$a = "SELECT users.photo AS PHOTO FROM users WHERE users.id = $user_id";
 $b = mysqli_query($con, $a)or die(mysqli_error($con));
 $row = mysqli_fetch_array($b);
 
